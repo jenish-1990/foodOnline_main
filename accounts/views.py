d1a91c4d5a70e66431ed6136f96459490b024d3c
jenish-1990 <ajakhed123@gmail.com>
2023-04-24 05:13:40 +0530

    User Registeration
    User Registeration
    Django Messages
    Error

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 # Create your views here.
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+
+            # Create the user using the form
+            # password = form.cleaned_data['password'] # get the password from the form
+            # user = form.save(commit=False) # form is now ready to be saved but not yet saved. whatever data we have in form will be assigned to user object
+            # user.set_password(password) # hashing the password
+            # user.role = User.CUSTOMER # now we assign the role of customer
+            # user.save()
+
+            # Create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, "Your account has been registered successfully!")
+            return redirect('registerUser')
+    
+    else:
+        form = UserForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'foodOnline_main/static'
@@ -136,3 +136,8 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
