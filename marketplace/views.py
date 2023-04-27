4920e1d4431e96c8b4144b86004210ca64b820b1
jenish-1990 <ajakhed123@gmail.com>
2023-04-28 01:45:42 +0530

    Opening Hours Full
    Using AJAX to implement opening hours
    Add Opening Hours
    Remove it
    Displaying them into MarketPlace

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,9 @@
+from datetime import date
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from marketplace.models import Cart
 from menu.models import Category, FoodItem
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from .context_processors import get_cart_counter, get_cart_amounts
 from django.contrib.auth.decorators import login_required
 
@@ -22,16 +23,25 @@ def marketplace(request):
     }
     return render(request, 'marketplace/listings.html', context)
 
+
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
 
-    categories = Category.objects.filter(vendor=vendor).order_by('category_name').prefetch_related(
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
         Prefetch(
             'fooditems',
-            queryset = FoodItem.objects.filter(is_available=True).order_by('food_title')
+            queryset = FoodItem.objects.filter(is_available=True)
         )
     )
 
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+    
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+    
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -41,6 +51,8 @@ def vendor_detail(request, vendor_slug):
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
